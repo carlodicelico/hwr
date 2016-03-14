4a7fb452698ea785627aa1203e81d5c6c2f8bb54
Carlo DiCelico <carlodicelico@me.com>
2016-03-14 11:47:45 -0400

    add ability to show forks and pulls as well as commits; clean up and refactor a bit; inlcude props message for illustrating props

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,37 +5,88 @@ class Detail extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { commits: [] };
+        this.state = {
+            mode: 'commits',
+            commits: [],
+            forks: [],
+            pulls:[]
+        };
     }
 
     componentWillMount() {
-        agent.get('https://api.github.com/repos/facebook/react/commits')
+        this.fetchFeed('commits');
+        this.fetchFeed('forks');
+        this.fetchFeed('pulls');
+    }
+
+    fetchFeed(type) {
+        agent.get(`https://api.github.com/repos/facebook/react/${type}`)
             .end((error, response) => {
-                if (!error && response) {
-                    console.dir(response.body);
-                    this.setState({ commits: response.body });
-                } else {
-                    console.log('There was an error fetching from GitHub', error);
-                }
+                if (!error && response) { this.setState({ [type]: response.body }); }
+                else { console.log(`Error fetching ${type}`, error); }
             }
         );
     }
 
-    render() {
-        return (
-            <div>
-            {this.state.commits.map((commit, index) => {
-                const author = commit.author ? commit.author.login : 'Anonymous',
-                    url = commit.html_url ? commit.html_url : '',
-                    message = commit.commit.message ? commit.commit.message : 'None';
+    selectMode(mode) { this.setState({ mode: mode }); }
 
-                return (
-                     <p key={index}>
-                         <strong>{author}</strong>:
-                         <a href={url}>{message}</a>
-                     </p>
-                );
-            })}
+    renderCommits() {
+        return this.state.commits.map((commit, index) => {
+            const author = commit.author ? commit.author.login : 'Anonymous';
+
+            return (
+                <p key={index}>
+                    <strong>{author}</strong>:
+                    <a href={commit.html_url}>{commit.commit.message}</a>.
+                </p>
+            );
+        });
+    }
+
+    renderPulls() {
+        return this.state.pulls.map((pull, index) => {
+            const author = pull.user ? pull.user.login : 'Anonymous';
+
+            return (
+                <p key={index}>
+                    <strong>{author}</strong>
+                    <a href={pull.html_url}>{pull.body}</a>.
+                </p>
+            );
+        });
+    }
+
+    renderForks() {
+        return this.state.forks.map((fork, index) => {
+            const author = fork.owner ? fork.owner.login : 'Anonymous';
+
+            return (
+                <p key={index}>
+                    <strong>{author}</strong>: forked to
+                    <a href={fork.html_url}>{fork.html_url}</a> at {fork.created_at}.
+                </p>
+            );
+        });
+    }
+
+    render() {
+        let content;
+
+        if (this.state.mode === 'pulls') {
+            content = this.renderPulls();
+        } else if (this.state.mode === 'forks') {
+            content = this.renderForks();
+        } else {
+            content = this.renderCommits();
+        }
+
+        return (
+             <div>
+                <h2>{this.props.message}</h2>
+                <button onClick={this.selectMode.bind(this, 'commits')}>Show Commits</button>
+                <button onClick={this.selectMode.bind(this, 'forks')}>Show Forks</button>
+                <button onClick={this.selectMode.bind(this, 'pulls')}>Show Pulls</button>
+                {content}
             </div>
         );
     }
